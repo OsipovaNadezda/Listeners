7253d58fb8981d7ef0c4f8b705e558012fbfcffe
OsipovaNadezda <osipavanadezda@gmail.com>
2022-07-01 13:20:50 +0300

    completed task1 lesson09

---
+++ Desktop/JS_lesson09/js/script.js
@@ -0,0 +1,41 @@
+let title = document.getElementsByTagName('h1')[0];
+console.log(title.textContent);
+
+let buttons = document.getElementsByClassName('handler_btn');
+console.log(buttons);
+const start = buttons[0];
+const reset = buttons[1];
+console.log(start);
+console.log(reset);
+
+const plus = document.querySelector('.screen-btn');
+console.log(plus.textContent)
+
+const percent = document.querySelectorAll('.other-items.percent');
+console.log(percent);
+
+const number = document.querySelectorAll('.other-items.number');
+console.log(number);
+
+const input = document.querySelector('div.rollback input[type=range]');
+console.log(input);
+
+const rangeValue = document.querySelector('div.rollback .range-value');
+console.log(rangeValue);
+
+const total = document.getElementsByClassName('total-input');
+const total1 = total[0];
+const total2 = total[1];
+const total3 = total[2];
+const total4 = total[3];
+const total5 = total[4];
+console.log(total);
+console.log(total1);
+console.log(total2);
+console.log(total3);
+console.log(total4);
+console.log(total5);
+
+let screens = document.querySelectorAll('.screen');
+console.log(screens);
+
